f97ce88845e848633b74209a8839ef6fccd6d3a0
Diego Oliveira de Araujo <diego.dearaujo@ambevtech.com.br>
2022-07-29 11:06:20 -0300

    login page obejcts

---
+++ cypress/integration/cucumber-tests/login/loginPage.js
@@ -0,0 +1,30 @@
+import cypress from "cypress";
+
+const URL = 'http://zero.webappsecurity.com/login.html';
+const USERNAME_INPUT = '#user_login';
+const PASSWORD_INPUT = '#user_password';
+const SUBMIT_BUTTON = 'input[name="submit"]';
+
+class LoginPage {
+  // visit webpage
+  static visit() {
+    cy.visit(URL);
+  }
+
+  // Fill username
+  static fillUsername(name) {
+    cy.get(USERNAME_INPUT).type(name);
+  }
+  // Fill password
+  static fillPassword(password) {
+    cy.get(PASSWORD_INPUT).type(password);
+  }
+
+  // Submit form
+  static submitForm() {
+    cy.get(SUBMIT_BUTTON).click();
+  }
+
+}
+
+export default LoginPage;
